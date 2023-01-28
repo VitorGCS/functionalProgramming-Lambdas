594615c8f2cf2dff996114e439c04baf15ed615c
vitor <vgc_silva@hotmail.com>
2023-01-28 20:18:17 +0000

    Coding Exercise 2
    Parameterized custom List & Iteratorrs

---
+++ src/generics/exercises/ListIterableParameterized.java
@@ -0,0 +1,19 @@
+package generics.exercises;
+
+/**
+ * Implementing this interface allows an object to returns
+ * object of ListIterator.
+ *
+ * @author Andrii Piatakha
+ *
+ */
+
+public interface ListIterableParameterized<T> {
+
+    /**
+     * Returns object of ListIterator.
+     *
+     * @return object of ListIterator
+     */
+    ListIteratorParameterized<T> listIterator();
+}
